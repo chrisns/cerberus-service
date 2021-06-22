25be99d46e46c4be076f1ecd0d2a0c3d9dacbbee
Chris Nesbitt-Smith <chris@cns.me.uk>
2021-06-22 22:50:28 +0100

    tidy run.sh

--- nginx/run.sh
+++ nginx/run.sh
@@ -2,12 +2,9 @@
 
 set -o errexit
 
-# mkdir -p /run/nginx
-
 # @TODO replace with envsubst
 
 # --- Start Insert ENV to JS bundle ---
-# Remember to also update webpack.config.js, Dockerfile and config.js
 # echo "== Inserting env variables =="
 for file in /usr/share/nginx/html/*.js
 do
@@ -19,13 +16,3 @@ do
   cat $file | sed 's,REPLACE_REFDATA_API_URL,'${REFDATA_API_URL}',g' > $file
 done
 # echo "== Finished ENV sub =="
-# --- End Insert ENV to JS bundle ---
-
-
-# echo "== Starting nginx using a config file =="
-# sed -i 's,REPLACE_CERBERUS_API_URL,'${CERBERUS_API_URL}',g' ${NGINX_CONFIG_FILE}
-# export CERBERUS_API_SERVER=`echo ${CERBERUS_API_URL} | awk -F/ '{print $3}'`
-# sed -i 's,REPLACE_CERBERUS_API_SERVER,'${CERBERUS_API_SERVER}',g' ${NGINX_CONFIG_FILE}
-# nginx -g 'daemon off;' -c ${NGINX_CONFIG_FILE}
-
-# /docker-entrypoint.sh
